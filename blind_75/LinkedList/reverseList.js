42f3d459d78c1b92f5d163cba890d15b2e094706
Kammal Shetty Sai Sumanth <sumanthksai@gmail.com>
2022-12-08 23:01:56 -0500

    reversing the list without recurrsion

---
+++ blind_75/LinkedList/reverseList.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    let newHead = null;
+    let current = head;
+    while(current){
+      tempNode =   current.next;
+      current.next = newHead; 
+      newHead = current;
+      current = tempNode  
+    }
+    return newHead
+   /* while(head.next!=null){
+        
+        head.next.next = head.next;
+        head.next = null;
+        newHead = head.next;
+    }*/
+    return newHead
+};
+
+function ListNode(val, next) {
+         this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+}
+
